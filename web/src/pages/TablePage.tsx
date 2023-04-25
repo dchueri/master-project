a04ed7732cfbfbaf512cea44c0543fdd7e22d778
diego <diegochueri@gmail.com>
2023-04-25 09:33:49 -0300

    feat(logout): add logout

--- web/src/pages/TablePage.tsx
+++ web/src/pages/TablePage.tsx
@@ -1,4 +1,6 @@
+import { useAuth } from 'context/AuthProvider/useAuth'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import FormModal from '../components/FormModal'
 import Table from '../components/Table'
 import Button from '../components/elements/Button'
@@ -10,15 +12,30 @@ const TablePage = () => {
   const handleCloseModal = (e: any) => {
     if (e.target.id === 'container') return setOpen(false)
   }
+  const auth = useAuth()
+  const navigate = useNavigate()
+
   return (
-    <div className="relative overflow-hidden bg-[white] h-[100vh] flex flex-col justify-center gap-2">
-      <div className="w-full flex justify-center">
-        <div onClick={handleOpenModal}>
-          <Button className="button text">Adicionar Projeto</Button>
-        </div>
-        <FormModal open={open} handleCloseModal={handleCloseModal} />
+    <div className="relative block overflow-hidden bg-[white] h-[100vh] justify-center gap-2">
+      <div className="h-[5vh] flex justify-end p-5">
+        <p
+          onClick={() => {
+            auth.logout()
+            navigate('/login')
+          }}
+        >
+          Sair
+        </p>
       </div>
-      <Table />
+      <div className="w-full flex items-end justify-center h-[20vh] pb-5">
+        <Button className="button text" onClick={handleOpenModal}>
+          Adicionar Projeto
+        </Button>
+      </div>
+      <div className="w-full flex items-start justify-center h-[75vh]">
+        <FormModal open={open} handleCloseModal={handleCloseModal} />
+        <Table />
+      </div>
       <img className="absolute top-[62%] left-[85%] h-[350px] z-0" src={Fred} />
     </div>
   )
