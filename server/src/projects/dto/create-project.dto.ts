8f93957fa09b255e853fc9615e6f53e7e052e949
diego <diegochueri@gmail.com>
2023-04-25 14:57:16 -0300

    feat(swagger): add swagger docs

--- server/src/projects/dto/create-project.dto.ts
+++ server/src/projects/dto/create-project.dto.ts
@@ -1,16 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsISO8601, IsInt, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateProjectDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   title: string;
   @IsInt()
   @IsNotEmpty()
+  @ApiProperty()
   zip_code: number;
   @IsISO8601()
   @IsNotEmpty()
+  @ApiProperty()
   deadline: string;
   @IsInt()
   @IsNotEmpty()
+  @ApiProperty()
   cost: number;
 }
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,12 +1,23 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: 'Cria um novo usuário' })
+  @ApiResponse({
+    status: 201,
+    description: 'Usuário criado',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Username já existente / Dados incompletos',
+  })
   @IsPublic()
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
